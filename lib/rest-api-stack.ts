d885d72f9e583fd917d71fe4688bfcb07e6d4ab7
Calvin Hurley <20017867@mail.wit.ie>
2024-02-26 17:58:30 +0000

    Exercise: Delete movie endpoint

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -7,6 +7,7 @@ import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { generateBatch } from "../shared/util";
 import { movies } from "../seed/movies";
+import * as apig from "aws-cdk-lib/aws-apigateway";
 
 export class RestAPIStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -53,6 +54,32 @@ export class RestAPIStack extends cdk.Stack {
           },
         }
         );
+
+      // Post Endpoint Function  
+      const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_16_X,
+        entry: `${__dirname}/../lambdas/addMovie.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: moviesTable.tableName,
+          REGION: "eu-west-1",
+        },
+      });
+
+      // Delete Endpoint Function  
+      const deleteMovieFn = new lambdanode.NodejsFunction(this, 'DeleteMovieFn', {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_16_X,
+        entry: `${__dirname}/../lambdas/deleteMovies.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: moviesTable.tableName,
+          REGION: "eu-west-1",
+        },
+      });
         
         new custom.AwsCustomResource(this, "moviesddbInitData", {
           onCreate: {
@@ -73,8 +100,48 @@ export class RestAPIStack extends cdk.Stack {
         // Permissions 
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
-        
-        
+        moviesTable.grantReadWriteData(newMovieFn)
+        moviesTable.grantWriteData(deleteMovieFn);
+
+        // Basic REST API 
+        const api = new apig.RestApi(this, "RestAPI", {
+          description: "demo api",
+          deployOptions: {
+            stageName: "dev",
+          },
+          defaultCorsPreflightOptions: {
+            allowHeaders: ["Content-Type", "X-Amz-Date"],
+            allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
+            allowCredentials: true,
+            allowOrigins: ["*"],
+          },
+        });
+
+        const moviesEndpoint = api.root.addResource("movies");
+        moviesEndpoint.addMethod(
+          "GET",
+          new apig.LambdaIntegration(getAllMoviesFn, { proxy: true })
+        );
+
+        // NEW
+        moviesEndpoint.addMethod(
+          "POST",
+           new apig.LambdaIntegration(newMovieFn, { proxy: true })
+        );
+
+        moviesEndpoint.addMethod(
+          "DELETE",
+          new apig.LambdaIntegration(deleteMovieFn, { proxy: true })
+        );
+
+        const movieEndpoint = moviesEndpoint.addResource("{movieId}");
+        movieEndpoint.addMethod(
+          "GET",
+          new apig.LambdaIntegration(getMovieByIdFn, { proxy: true })
+        );
+
+
+
       }
     }
     
